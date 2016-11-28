f63d3bde284e7f25ffc661cd2acb9b31c9d8825e
charles West <charleswest33@gmail.com>
2016-11-27 19:26:31 -0600

    added another function to call 10 objects but no Go

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-var channelName = 'hw7youtube';
+var channelName = 'motownapi';
+
 $(document).ready(function(){
 	$.get(
 		"https://www.googleapis.com/youtube/v3/channels",{
 			part:"contentDetails",
 			forUsername: channelName,
-			key: 'AIzaSyDnWkipxceKbNQrgHNv6-KFcq_XgNHQu1Q'},
+			key: 'AIzaSyA5IsV26Naq5SWUIqLiSxJEHTAyWZyfA4k'},
 			function(data){
 				$.each(data.items, function(i, item){
 					console.log(item);
+					pid = item.contentDetails.relatedPlaylist.uploads;
+					getVids(pid);
 				})			
 			}
 		);
+	function getVids(pid){
+		$.get(
+		"https://www.googleapis.com/youtube/v3/channels",{
+			part:"snippet",
+			maxResults: 10,
+			relatedPlayId: pid,
+			key: 'AIzaSyA5IsV26Naq5SWUIqLiSxJEHTAyWZyfA4k'},
+			function(data){
+				var output;
+				$.each(data.items, function(i, item){
+					console.log(item);
+					videoTitle = item.snippet.title;
+
+					output = '<li>'+vidTitle+'</li>';
+
+					//Append to results 
+					$('#results').append(output);
+
+				})			
+			}
+		);
+	}
 });
+// $('#myVideoButton').onClick(function () {
+	// API code
+//  })
